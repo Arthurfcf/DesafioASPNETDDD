05e70daa76650b7009e80aa71f8b5fb2c48cbc73
Arthur Ferreira <arthur.ferreira@compasso.com.br>
2022-01-03 20:08:38 -0300

    Subindo aplicação

---
+++ Controllers/FuncionariosController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Aplicacao.Controllers
+{
+    public class FuncionariosController : Controller
+    {
+        public IActionResult Index()
+        {
+           
+        }
+    }
+}
